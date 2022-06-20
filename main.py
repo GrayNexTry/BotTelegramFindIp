11e5322fdcb9ccce0cf68a8f6ee53fcea0b1cd08
GrayNexTry <www.nextry@gmail.com>
2022-06-20 23:45:52 +0700

    !

--- main.py
+++ main.py
@@ -24,14 +24,12 @@ def chek_IP(ip, message):
     except socket.error:
         try:
             myHostName = socket.gethostname(ip)
-            
-            responce = requests.get(url=f'http://ip-api.com/json/{myHostName }').json()
         
             answer = bot.send_message(message.chat.id, "Запрос обрабатывается...")
             bot.edit_message_text(f"Страна: {responce.get('country')} \nРегион: {responce.get('regionName')} \nГород: {responce.get('city')} \nПочтовый индекс: {responce.get('zip')} \nЧасовой пояс: {responce.get('timezone')} \nПровайдер: {responce.get('isp')}", chat_id=message.chat.id, message_id=answer.id)
             bot.send_location(message.chat.id, latitude=responce.get('lat'), longitude=responce.get('lon'))
             
         except socket.error:
-            bot.send_message(message.chat.id, "Не правильный домен или ip")
+            bot.send_message(message.chat.id, "Не правильный домен или IP")
     
 bot.polling(non_stop=True)
